b35a30ee6a433b8849a3e8c6398511144e53f645
Egordgan <egor.mihalkin2017@yande.ru>
2020-08-15 18:28:30 +0400

    feat: add js function for modal

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {       КОД НА jQUERY
+                                    
+// });
+document.addEventListener("DOMContentLoaded", function(event){
+  const modal = document.querySelector('.modal');
+  const modalBtn = document.querySelectorAll('[data-toggle=modal]');
+  const closeBtn = document.querySelector('.modal__close');
+  const switchModal = () =>{
+    modal.classList.toggle('modal--visible');
+  }
+
+  modalBtn.forEach(element => {
+    element.addEventListener('click', switchModal); 
+  });
+  closeBtn.addEventListener('click', switchModal);
+
+});
